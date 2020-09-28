18b1925cb60050f9e4e19af6f85a83931240c910
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-28 20:44:43 +0000

    express server file v.3

--- express_server.js
+++ express_server.js
@@ -11,8 +11,14 @@ app.get("/", (request, response) => {
   response.send("Hello");
 }); 
 
+// add json
 app.get("/urls.json", (request,response) => {
   response.json(urlDatabase);
+});
+
+// add html
+app.get("/hello", (request,response) => {
+  response.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {

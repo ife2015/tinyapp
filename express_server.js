d1cf3820d2e69ef0513ed90b37a89debfd97c30e
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-28 22:29:46 +0000

    Added ejs view engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express(); 
 const PORT = 8080; 
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,12 +13,12 @@ app.get("/", (request, response) => {
   response.send("Hello");
 }); 
 
-// add json
+// add json -> we can send stringed objects
 app.get("/urls.json", (request,response) => {
   response.json(urlDatabase);
 });
 
-// add html
+// add html -> we can send html responses
 app.get("/hello", (request,response) => {
   response.send('<html><body>Hello <b>World</b></body></html>\n');
 });

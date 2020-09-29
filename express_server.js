63d94e9966d44833b065f7e31642e2174f058a00
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-29 01:15:18 +0000

    added POST to handle form

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express(); 
+const bodyParser = require("body-parser");
 const PORT = 8080; 
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,6 +25,16 @@ app.get('/urls', (request,response) => {
   response.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (request,response) => {
+  
+  response.render('urls_new');
+});
+
+app.post("/urls", (request, response) => {
+  console.log(request.body); 
+  res.send("Ok");         
+});
+
 app.get("/urls/:shortURL", (request, response) => {
   const templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL]};
   response.render("urls_show", templateVars);
@@ -32,6 +44,7 @@ app.get("/urls/:shortURL", (request, response) => {
 app.get("/hello", (request,response) => {
   response.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`); 

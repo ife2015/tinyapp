9d5aab1b935c6feb8395a73a984d5c233cbc7d61
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-01 21:10:51 +0000

    hashed the password and included helper functions

--- express_server.js
+++ express_server.js
@@ -3,8 +3,10 @@ const cookieParser = require('cookie-parser');
 const app = express(); 
 const bodyParser = require("body-parser");
 const PORT = 8080; 
+const bcrypt = require('bcrypt'); 
 
 app.set('view engine', 'ejs');
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -18,22 +20,22 @@ const generateRandomString = function() {
   return text;
 };
 
-const lookupEmails = function(userDatabase,useremail) {
-  for(let id in userDatabase) {
-    if(users[id].email === useremail) {
+const lookupEmails = function(userDatabase, useremail) {
+  for (let id in userDatabase) {
+    if (users[id].email === useremail) {
       return true;
     }
   }
 }; 
 
-const urlsForUser = function(id) {
-  let urlsData = {}; 
-  for(let user in urlDatabase) {
-    if(urlDatabase[user].userID === id) {
-      urlsData[user]= urlDatabase[user].longURL; 
+const urlsForUser = function (id) {
+  let urlsData = {};
+  for (let user in urlDatabase) {
+    if (urlDatabase[user].userID === id) {
+      urlsData[user] = urlDatabase[user].longURL;
     }
   }
-  return urlsData; 
+  return urlsData;
 };
 
 //generateRandomString(); 
@@ -46,9 +48,28 @@ const users = {
   'aJ48lW': {id: 'aJ48lW', email:"ifemastero@yahoo.com", password:"zxc" } 
 };
 
+const findUserByEmail = function(email) {
+  for (let userId in users) {
+    if (email === users[userId].email) { 
+      return users[userId];
+    }
+  }
+  return false; 
+};
+
+const authenticateUser = function(email, password) {
+  const user = findUserByEmail(email); 
+  
+  if(user && bcrypt.compareSync(password, user.password)) {
+    return user.id;
+  }
+  return false; 
+};
+
 app.get("/", (request, response) => {
   response.send("Hello");
 }); 
+
 
 // add json -> we can send stringed objects
 app.get("/urls.json", (request,response) => {
@@ -62,7 +83,7 @@ app.get('/urls', (request,response) => {
   };
 
   //CHECKs usersId in urldatabase and compares to userlogin
-  if(request.cookies["user_id"] === undefined) {
+  if (request.cookies["user_id"] === undefined) {
     //login 
     response.redirect('/login');
   } else {
@@ -95,7 +116,8 @@ app.get('/urls/:shortURL', (request, response) => {
   
     response.render('urls_show', templateVars);
   });
-  
+
+    
   // short url link is redirected to the long url
   app.get('/u/:shortURL', (request, response) => {
     const longURL = urlDatabase[request.params.shortURL]; 
@@ -155,23 +177,27 @@ app.post('/urls', (request, response) => {
   }
 });  
 
-// edit login 
-app.post('/login', (request,response) => {
-  const {email, password } = request.body;
 
-  for(let user in users) {
-    if(email === users[user].email) {
-      if(password === users[user].password) {
-          response.cookie('user_id', user);
-          response.redirect('/urls'); 
-      }
-    } else {
-      response.status(403).send("Email or Password don't match"); 
-    }
-   }
-   response.status(403).send("Your email was not found!"); 
+
+
+// edit login 
+// bcrypt.compareSync(password, users[user].password)
+app.post('/login', (request, response) => {
+  const { email, password } = request.body;
+  console.log(request.body);
+
+  const userId = authenticateUser(email,password); 
+
+  if(!userId) {
+    response.status(403).send("Email or Password don't match");
+  } else {
+    response.cookie('user_id', userId);
+    response.redirect('/urls');
+  }
 
 });
+
+
 
 // edit logout
 app.post('/logout', (request,response) => {
@@ -179,24 +205,31 @@ app.post('/logout', (request,response) => {
   response.redirect('/urls');
 });
 
+
+
 // edit registration 
-app.post('/register', (request,response) => {
-  const {email, password } = request.body;
+app.post('/register', (request, response) => {
+  const { email, password } = request.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
   const userID = generateRandomString();
-  
-  if(email === "" || password === "") {
+
+  if (email === "" || password === "") {
     response.status(400).send("Email or Password is empty");
     return;
-  } 
-  
-  if (lookupEmails(users,email) === true) {
-   response.status(400).send("Email is already in the system!");
-   return;
   }
-    users[userID] = {id:userID,email,password};
-    response.cookie('user_id',userID);
-    response.redirect('/urls');
+
+  if (lookupEmails(users, email) === true) {
+    response.status(400).send("Email is already in the system!");
+    return;
+  }
+  users[userID] = { id: userID, email, password:hashedPassword};
+  console.log(users);
+  response.cookie('user_id', userID);
+  response.redirect('/urls');
 });
+
+
+
 
 
 app.listen(PORT, () => {

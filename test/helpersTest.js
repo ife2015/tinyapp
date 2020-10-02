a2474323729dd11c8d7d6e8213eac050ac171edf
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-02 02:23:51 +0000

    test function for helper functions

---
+++ test/helpersTest.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+
+const getUserByEmail = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail(testUsers, "user@example.com");
+    const expectedOutput = "userRandomID";
+    // Write your assert statement here
+    assert.equal(user,expectedOutput);
+  });
+
+  it('should return if undefined with a non-registered email', function() {
+    const user = getUserByEmail(testUsers, "user3@example.com");
+    assert.isUndefined(user);
+  });
+
+});
